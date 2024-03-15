db639cb7bc5b86d1566345fc1a2acc9cf945f5b4
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-03-15 20:03:55 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountName = "Raj Aryan"
+var accountEmail = "raj@google.com"
+accountCity = "Jaipur"
+
+// accountId = 244553 // not allowed
+
+accountName = "Gungun Singh"
+accountEmail = "gungun@gmail.com"
+accountCity = "Patna"
+
+console.log(accountId)
+console.table([accountId, accountName, accountEmail, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" //treats all code as newer version of JS
+
+// alert("Good Morning") // gives an alert box in browser, not here in nodejs env
+
+let name = "Raj Aryan"
+let age = 22
+let isLoggedIn = false
+
+//number => range upto 2 to power 53
+// bigint for very big numbers
+//string like let name => "" , ''
+// boolean => true / false
+// null => quite imp, is a standalone value, empty value (assigned2)
+// undefined => jab value define hi nhi variable ko
+//symbol => mostly used to find uniqueness of individual elements
+
+
+//object
+
+console.log(typeof "raj aryan");  //string
+console.log(typeof age);          //int
+console.log(typeof undefined);    //undefined
+console.log(typeof null);         //object      ***
