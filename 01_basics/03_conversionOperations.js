d413c0bd1910476fe698d64d2e282768d2b05b16
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-03-16 20:12:45 +0000

    commit

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = 33
+
+console.log (typeof score)
+console.log (typeof (score))
+
+let valueInNumber = String (score)  //Number(score) : changes to nmbr dataType
+
+console.log (typeof valueInNumber)
+
+
+let marks = "69"
+console.log (typeof marks)
+
+let convertToNmbr = Number (marks)
+console.log (typeof convertToNmbr)
+console.log (marks)
+
+
+let abc = null
+console.log(abc)
+console.log(typeof abc)
