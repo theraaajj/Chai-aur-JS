7b7c2cdf5c401ba7c502cc483b35a0d682d31f9a
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-03-18 07:50:31 +0000

    commit

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,21 +1,52 @@
 let score = 33
 
-console.log (typeof score)
-console.log (typeof (score))
+//console.log (typeof score)
+//console.log (typeof (score))
 
 let valueInNumber = String (score)  //Number(score) : changes to nmbr dataType
 
-console.log (typeof valueInNumber)
+//console.log (typeof valueInNumber)
 
 
 let marks = "69"
-console.log (typeof marks)
+//console.log (typeof marks)
 
 let convertToNmbr = Number (marks)
-console.log (typeof convertToNmbr)
-console.log (marks)
+//console.log (typeof convertToNmbr)
+//console.log (marks)
 
 
 let abc = null
-console.log(abc)
-console.log(typeof abc)
+//console.log(abc)
+//console.log(typeof abc)
+
+
+// ************ Operations **************** //
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+//*******String Concatenation ********/
+let str1 = "hello"
+let str2 = " raj"
+let str3 = str1 + str2
+// console.log (str3);
+
+// console.log ("1" + 2)         //anything added to string will be 
+// console.log (1 + "2")         //implicitly converted to string during it's operations
+// console.log ("1" + 2 + 2)     //addition starts from the left so same here
+// console.log (1 + 2 + "2")     //from left, therefore phle integers add hokr 3 ban gye, and then added to string
+
+// console.log(+true)     //1
+// console.log(+"")       //0
+// console.log(+false)    //0
+
+let gameCounter = 100
+gameCounter++                    //prefix aur postfix dono hi hota hai (++gameCounter)
+console.log(gameCounter);
