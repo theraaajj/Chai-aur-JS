e5aefd72c2249ac7a3782085efdf47065fe67c6d
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-03-21 20:07:07 +0000

    commit

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,1 +1,19 @@
-const name = "Raj"
+//  const name = "Raj"
+//  const repoCount = 10
+
+// // console.log (name + repoCount + " Value")   //concatenation
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);     //string interpolation
+
+//another way to declare a string
+const myname =  new String ("RAJ ARYAN")
+
+//treated as an object where the string's letters are indexed from 0 to n. length func can be invoked. and multiple other methods are also availed.
+
+// console.log (myname[0]);
+// console.log (myname.__proto__);
+
+// console.log (myname.length);
+// console.log(myname.toLowerCase());
+console.log(myname.charAt(5));
+console.log(myname.indexOf("N"));
