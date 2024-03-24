03a3858e0e769a9cfe718e4b2170cfa54abb5603
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-03-24 18:10:27 +0000

    commit

--- 01_basics/06_nums_maths.js
+++ 01_basics/06_nums_maths.js
@@ -1,5 +1,12 @@
 const score = 400
 console.log(score);
 
+//when declaring as an object
 const balance = new Number(500)
 console.log(balance);
+//specifies it to be a number and thereby few functions can be applied.
+
+// console.log(balance.toString());               //converts to string and therefore strings functions are availed.
+console.log(balance.toString().length);
+
+
