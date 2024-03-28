5b0f562260b237454d18c9b2cd210b40aaa031ca
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-03-28 18:15:00 +0000

    commit

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+// Here we start arrays in JS
+
+//arrays in JS are resizeable: can add or delete elements
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros = ['ironman', 'spiderman', 'flash']
+// console.log(myArray[1]);
+
+//another way to declare an array as an object
+const myArray2 = new Array(1, 2, 3, 4)
+// console.log(myArray2[0]);
+
+// Array Methods
+
+// myArray.push(6)                 //adds 6 in the array
+// console.log(myArray)
+// myArray.pop()                   //removes last element from array
+// console.log(myArray)
+
+// myArray.unshift(9)                 //adds this elem at begin of the array and shift each element by one place. but not advised to use as it would be a big load in a large array
+// console.log(myArray)
+// myArray.shift()                    //removes element from start of the array (no parameters req.)
+// console.log(myArray)
+
+// console.log(myArray.includes(9))     //whether array has 9 or not? (true/false)
+// console.log(myArray.indexOf(3))
+
+// const wowArray = myArray.join()         //gives the same value but as a string and not an array
+// console.log(myArray)
+// console.log(wowArray)
+
+
+//slice and splice
+// check it out yourself
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,34 @@
+const mcu = ['ironman', 'captain america', 'hulk', 'thor']
+const dceu = ['superman', 'batman', 'aquaman', 'wonder woman']
+
+// mcu.push(dceu)                    //adds the whole dceu array as a single element at the last of mcu array 
+// console.log(mcu);
+
+//now to access element from inside this dceu array, the syntax should be:
+// console.log(mcu[4][3]);           //not considered a good way to access!
+
+// if only the values are to be merged, we can use concat()
+// const allHeros = mcu.concat(dceu)
+// console.log(allHeros);
+
+// this same thing is done easily by a spread operator, eithout a limitation to the no. of arrays to be merged
+const allNewHeros = [...mcu, ...dceu]        //due to dots all elements are spread in the new array
+console.log(allNewHeros);
+
+const myArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]           //a multi-depth array
+// const anotherArray = myArray.flat(Infinity)                    //with a capital I
+// const anotherArray2 = myArray.flat(1)
+const anotherArray3 = myArray.flat(2)
+console.log(anotherArray3);                                        //Infinity removes any nmbr of depths present, else can give a numeric value upto which depths are to be removed.
+
+
+console.log(Array.isArray("rajaryan"))
+console.log(Array.from("rajaryan"))                                //forms an array of each letter as an element
+console.log(Array.from({name:'rajaryan'}))                         //interesting case, can't convert to array asa it's a key:value pair, therefore just gives an empty array
+
+
+
+const score1 = 100
+const score2 = 200
+const score3 = 300
+console.log(Array.of(score1, score2, score3))                      // [ 100, 200, 300 ] : diff variables/values ka ek array
