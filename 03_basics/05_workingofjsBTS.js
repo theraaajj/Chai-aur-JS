8a28953a7f4475e2d59b2c2f35712476cbbc5089
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-04-20 17:50:06 +0000

    commit

---
+++ 03_basics/05_workingofjsBTS.js
@@ -0,0 +1,6 @@
+// JavaScript Execution Context : mtlb JS aapke created file ko execute kaise karti hai??
+//                                do phase me run karti hai
+
+// 1. Global EC
+// 2. Function EC
+// 3. Eval EC
