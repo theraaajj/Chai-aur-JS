0d5120d1b5ba27e3022d4f35884ceefdb3dd57a1
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-04-21 15:49:53 +0000

    commit

--- 03_basics/05_workingofjsBTS.js
+++ 03_basics/05_workingofjsBTS.js
@@ -1,5 +1,8 @@
 // JavaScript Execution Context : mtlb JS aapke created file ko execute kaise karti hai??
-//                                do phase me run karti hai
+//                                do phase me run karti hai humesa.
+// sbse pehle ek global execution context banta HTMLDetailsElement. aur usko refer krdiya jata hai 'this' global variable se!
+
+// javascript single threaded language hai, isliye saari chizen ek process me execute hoti hain..
 
 // 1. Global EC
 // 2. Function EC
