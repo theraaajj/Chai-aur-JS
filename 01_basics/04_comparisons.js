8f9b2220bbd904fa2c6b6241f6e6d0374ab1db67
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2024-04-23 23:49:03 +0530

    04_comparisons.js

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -4,13 +4,13 @@
 // console.log (2==1)
 // console.log (2!=1)
 
-// console.log("2">1)     //true
-// console.log("02">1)    //true
+// console.log("2">1)         //true
+// console.log("02">1)        //true
 
-console.log(null>0)      //false
-console.log(null==0)     //false
-console.log(null>=0)     //true : jab 2 comparison operator lag jate toh null ko nmbr(0)
-                         //ki tarah treat karte h, i.e. null (0) >= 0 is true
+console.log(null>0)           //false
+console.log(null==0)          //false
+console.log(null>=0)          //true : jab 2 comparison operator lag jate toh null ko nmbr(0)
+                              //ki tarah treat karte h, i.e. null (0) >= 0 is true
 
 console.log(undefined>0)      //for undefined aisa nhi hota, therefore false
 console.log(undefined==0)
